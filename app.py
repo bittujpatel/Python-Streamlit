ef6a193b0263ecdcc47b0eca0fd634c809b12894
Chandra Prakash J Patel <90178452+bittujpatel@users.noreply.github.com>
2022-04-05 02:27:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
-# import plotly.express as px
+import plotly.express as px
 
 DATA_URL = (
     "https://data.cityofnewyork.us/resource/h9gi-nx95.csv"
@@ -71,8 +71,8 @@ filtered = data[
 hist = np.histogram(filtered[DATE_TIME].dt.minute, bins=60, range=(0, 60))[0]
 chart_data = pd.DataFrame({"minute": range(60), "crashes": hist})
 
-# fig = px.bar(chart_data, x='minute', y='crashes', hover_data=['minute', 'crashes'], height=400)
-# st.write(fig)
+fig = px.bar(chart_data, x='minute', y='crashes', hover_data=['minute', 'crashes'], height=400)
+st.write(fig)
 
 st.header("Top 5 dangerous streets by affected class")
 select = st.selectbox('Affected class', ['Pedestrians', 'Cyclists', 'Motorists'])
